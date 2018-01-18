d6f47cf8f95ecb995c721d8032c6f1658bcc3a08
dead-beef <dead-beef@users.noreply.github.com>
2018-01-18 08:40:38 +0000

    fix commands

--- modules/commands.mk
+++ modules/commands.mk
@@ -7,7 +7,7 @@ CAT = $(SHELLJS) cat
 #CPDIST = $(SHELLJS) cp -f
 CPDIST = $(SHELLJS) ln -f
 MKDIR = $(SHELLJS) mkdir -p
-ECHO = $(SHELLJS) echo
+ECHO = echo
 
 PRINTF = printf
 RESET_MAKE = env -u MAKELEVEL -u MAKEFILES $(MAKE)
@@ -16,9 +16,6 @@ NODE = node
 
 MAKE_VARS = $(MAKEFILE_DIR)/bin/make-vars
 MAKE_VARS_CMD = $(MAKE_VARS) $(OVERRIDE_CONFIG_FILE)
-
-# $(call LINTJS, files)
-LINTJS = eslint $(1)
 
 #VER := $(shell ver)
 #ifeq "$(findstring Windows, $(VER))" "Windows"
