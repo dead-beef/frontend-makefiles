cb66e2b5496f9696a3fd1af60816bff58d99e578
dead-beef <dead-beef@users.noreply.github.com>
2018-01-27 23:38:09 +0000

    change default variables

--- modules/utils.mk
+++ modules/utils.mk
@@ -13,12 +13,18 @@ QUOTE2 := \"
 # $(call load-modules, modules)
 load-modules = $(eval include $(addprefix $(MAKEFILE_DIR)/modules/,\
                                           $(addsuffix .mk,$1)))
+# $(call main)
+main = $(call load-modules, main info test)
+# $(call set-default, var, default_value)
+set-default = $(eval $(1) := $$(if $$($(1)),$$($(1)),$2))
 # $(call join-with, separator, list)
 join-with = $(subst $(SPACE),$1,$(strip $2))
 # $(call uniq, list)
 uniq = $(if $1,$(firstword $1) $(call uniq,$(filter-out $(firstword $1),$1)))
 # $(call get-dirs, paths)
 get-dirs = $(call uniq,$(foreach d,$1,$(dir $d)))
+# $(call src-to-min, paths)
+src-to-min = $(patsubst %.js,%.min.js,$(patsubst %.css,%.min.css,$1))
 
 MKDIRS :=
 mkdirs = $(eval MKDIRS += $1)
@@ -39,4 +45,9 @@ endef
 define rwildcards
 	$(sort $(foreach p,$2,$(wildcard $1$p))) \
 	$(foreach d,$(sort $(wildcard $1*)),$(call rwildcards,$d/,$2))
+endef
+
+# $(call add-include-path, paths)
+define add-include-path
+$(eval INCLUDE_PATH := $(call join-with,:,$(1) $(INCLUDE_PATH)))
 endef
--- modules/vars.mk
+++ modules/vars.mk
@@ -1,16 +1,13 @@
-ifeq "$(CONFIG_FILE)" ""
-CONFIG_FILE := config/app.mk
-endif
+$(call set-default,OVERRIDE_CONFIG_FILE,config/override.js)
+$(call set-default,BUILD_DIR,build)
+$(call set-default,DEP_DIR,$(BUILD_DIR)/deps)
+$(call set-default,MIN_DIR,$(BUILD_DIR)/min)
+$(call set-default,DIST_DIR,dist)
 
-ifeq "$(OVERRIDE_CONFIG_FILE)" ""
-OVERRIDE_CONFIG_FILE := config/override.js
-endif
-
-BUILD_DIR = build
-DEP_DIR := $(BUILD_DIR)/deps
-MIN_DIR := $(BUILD_DIR)/min
-DIST_DIR = dist
 LINT_ENABLED := 1
+
+SERVER_IP := 127.0.0.1
+SERVER_PORT := 8080
 
 TARGETS :=
 NPM_SCRIPTS :=
@@ -18,11 +15,12 @@ LOAD_MODULES :=
 VARS := MODULE_PATH VARS_FILE MAKEFILES TARGETS NPM_SCRIPTS
 
 VARS_FILE := $(BUILD_DIR)/vars.mk
-MAKEFILES := Makefile $(VARS_FILE) $(wildcard $(MAKEFILE_PATH)/*.mk) \
-             $(CONFIG_FILE)
+MAKEFILES := Makefile $(VARS_FILE) $(wildcard $(MAKEFILE_PATH)/*.mk)
 MODULE_DIRS := node_modules
 
 -include $(VARS_FILE)
+
+INCLUDE_PATH := $(call join-with,:,$(MODULE_PATH))
 
 ifneq "$(MAKECMDGOALS)" "install"
 $(VARS_FILE): package.json $(MAKE_VARS) $(OVERRIDE_CONFIG_FILE) | $(BUILD_DIR) $(MODULE_DIRS)
